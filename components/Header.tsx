cb3935b6d2ad6c50434905247405605b0e0fa497
Subho <contact.subhoghosh@gmail.com>
2022-09-14 17:51:49 +0530

    feat: add header & enable darkmode using class

---
+++ components/Header.tsx
@@ -0,0 +1,78 @@
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <header className="px-8 py-6 flex items-center relative">
+      <Link href="/">
+        <a className="font-bold mr-auto">Subho Ghosh</a>
+      </Link>
+
+      <div className="flex items-center gap-x-3">
+        {/* TODO: make the menu better */}
+        <nav
+          className={`absolute right-6 top-4 bg-gray-300 dark:bg-gray-900 px-6 py-12 rounded-2xl flex flex-col gap-y-4 w-48 ${
+            isOpen ? "" : "hidden"
+          }`}
+        >
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
+        </nav>
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+            />
+          </svg>
+        </button>
+        <button
+          className="p-1 hover:bg-gray-300 dark:hover:bg-gray-900 rounded relative"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          )}
+        </button>
+      </div>
+    </header>
+  );
+}
