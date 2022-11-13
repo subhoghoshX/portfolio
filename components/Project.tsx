65103910fe45ce8a1df140897833ce5f673e5e33
Subho <contact.subhoghosh@gmail.com>
2022-11-13 20:53:14 +0530

    feat: revamp projects section

--- components/Project.tsx
+++ components/Project.tsx
@@ -8,27 +8,35 @@ export default function Project({
   isOpenSource,
 }) {
   return (
-    <div className="md:grid md:grid-cols-12 md:items-center md:gap-x-8">
-      <img
-        className="aspect-video rounded-lg md:col-span-5 md:h-full"
-        src={previewUrl}
-        alt={alt}
-      />
+    <div className="md:grid md:grid-cols-12 md:items-center md:gap-x-14">
+      <div className="md:col-span-5 relative">
+        <img
+          className="aspect-video rounded-lg md:h-full"
+          src={previewUrl}
+          alt={alt}
+        />
+        <div>
+          <div className="absolute -top-4 -right-12 -left-12 h-px bg-slate-900/[0.1] dark:bg-cyan-900 [mask-image:linear-gradient(to_right,transparent,white_4rem,white_calc(100%-4rem),transparent)]"></div>
+          <div className="absolute -top-12 -bottom-12 -left-4 w-px bg-slate-900/[0.1] dark:bg-cyan-900 [mask-image:linear-gradient(to_top,transparent,white_4rem,white_calc(100%-4rem),transparent)]"></div>
+          <div className="absolute -top-12 -bottom-12 -right-4 w-px bg-slate-900/[0.1] dark:bg-cyan-900 [mask-image:linear-gradient(to_top,transparent,white_4rem,white_calc(100%-4rem),transparent)]"></div>
+          <div className="absolute -bottom-4 -right-12 -left-12 h-px bg-slate-900/[0.1] dark:bg-cyan-900 [mask-image:linear-gradient(to_right,transparent,white_4rem,white_calc(100%-4rem),transparent)]"></div>
+        </div>
+      </div>
       <div className="mt-4 md:col-span-7 md:mt-0">
         <div>
           <h4 className="text-xl font-bold md:text-base">{title}</h4>
-          <p className="mt-2 text-gray-700 md:text-sm">{desc}</p>
+          <p className="mt-2 text-zinc-400 md:text-sm">{desc}</p>
         </div>
         <div className="mt-4 space-x-5 text-white">
           <a
             href={liveUrl}
-            className="inline-block rounded bg-gray-900 px-12 py-2"
+            className="inline-block rounded bg-zinc-700 px-12 py-2"
           >
             View Live
           </a>
           <a
             href={codeUrl}
-            className="inline-block rounded bg-gray-900 px-12 py-2"
+            className="inline-block rounded bg-zinc-700 px-12 py-2"
           >
             {isOpenSource ? "My Contrbutions" : "Code"}
           </a>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,15 +4,20 @@ export default function Projects() {
   return (
     <article className="mt-20" id="projects">
       <h2 className="text-center text-4xl font-bold sm:text-5xl">Projects</h2>
-      <h3 className="mt-8 text-sm text-blue-800">Personal projects:</h3>
-      <p className="mt-2">
-        I love to build something in my free time. Below are some of my
-        tinkerings.
-      </p>
+      <h3 className="mt-20 text-sm text-cyan-400 border border-cyan-800 inline-block px-4 py-1.5 rounded-full bg-cyan-900/30">Personal Projects</h3>
       <div className="mt-10 space-y-14">
         <Project
+          title="Laurete"
+          desc="A blazing fast application to convert your tweet into beautiful images. Have all the configuration options you need at your fingertip. Simple by default, powerful when neeeded."
+          previewUrl="/laureate.png"
+          alt="preview"
+          liveUrl="#"
+          codeUrl="https://github.com/subhoghoshX/portfolio"
+          isOpenSource={false}
+        />
+        <Project
           title="An out of the world project"
-          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut ex mi. Nulla a lacinia nisl. Quisque scelerisque velit elit, id imperdiet ipsum malesuada maximus. Aliquam eleifend ut sem id interdum. Duis ornare mollis pretium. Vestibulum finibus sem non posuere accumsan. Ut elementum ornare suscipit."
+          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut ex mi. Nulla a lacinia nisl. Quisque scelerisque velit elit, id imperdiet ipsum malesuada maximus."
           previewUrl="https://via.placeholder.com/1600x900"
           alt="preview"
           liveUrl="#"
@@ -21,16 +26,7 @@ export default function Projects() {
         />
         <Project
           title="An out of the world project"
-          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut ex mi. Nulla a lacinia nisl. Quisque scelerisque velit elit, id imperdiet ipsum malesuada maximus. Aliquam eleifend ut sem id interdum. Duis ornare mollis pretium. Vestibulum finibus sem non posuere accumsan. Ut elementum ornare suscipit."
-          previewUrl="https://via.placeholder.com/1600x900"
-          alt="preview"
-          liveUrl="#"
-          codeUrl="https://github.com/subhoghoshX/portfolio"
-          isOpenSource={false}
-        />
-        <Project
-          title="An out of the world project"
-          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut ex mi. Nulla a lacinia nisl. Quisque scelerisque velit elit, id imperdiet ipsum malesuada maximus. Aliquam eleifend ut sem id interdum. Duis ornare mollis pretium. Vestibulum finibus sem non posuere accumsan. Ut elementum ornare suscipit."
+          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut ex mi. Nulla a lacinia nisl. Quisque scelerisque velit elit, id imperdiet ipsum malesuada maximus."
           previewUrl="https://via.placeholder.com/1600x900"
           alt="preview"
           liveUrl="#"
@@ -38,17 +34,18 @@ export default function Projects() {
           isOpenSource={false}
         />
       </div>
-      <h3 className="mt-20 text-sm text-blue-800">
+      <h3 className="mt-32 text-sm text-cyan-400 border border-cyan-800 inline-block px-4 py-1.5 rounded-full bg-cyan-900/30">Opensource Contributions</h3>
+{/*      <h3 className="mt-20 text-sm text-blue-800">
         Open source contributions:
       </h3>
       <p className="mt-2">
         Apart from bringing my own ideas to life, I also enjoy exploring and
         participating in community projects.
       </p>
-      <div className="mt-10 space-y-14">
+*/}      <div className="mt-10 space-y-14">
         <Project
           title="An out of the world project"
-          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut ex mi. Nulla a lacinia nisl. Quisque scelerisque velit elit, id imperdiet ipsum malesuada maximus. Aliquam eleifend ut sem id interdum. Duis ornare mollis pretium. Vestibulum finibus sem non posuere accumsan. Ut elementum ornare suscipit."
+          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut ex mi. Nulla a lacinia nisl. Quisque scelerisque velit elit, id imperdiet ipsum malesuada maximus."
           previewUrl="https://via.placeholder.com/1600x900"
           alt="preview"
           liveUrl="#"
@@ -57,7 +54,7 @@ export default function Projects() {
         />
         <Project
           title="An out of the world project"
-          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut ex mi. Nulla a lacinia nisl. Quisque scelerisque velit elit, id imperdiet ipsum malesuada maximus. Aliquam eleifend ut sem id interdum. Duis ornare mollis pretium. Vestibulum finibus sem non posuere accumsan. Ut elementum ornare suscipit."
+          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut ex mi. Nulla a lacinia nisl. Quisque scelerisque velit elit, id imperdiet ipsum malesuada maximus."
           previewUrl="https://via.placeholder.com/1600x900"
           alt="preview"
           liveUrl="#"
@@ -66,7 +63,7 @@ export default function Projects() {
         />
         <Project
           title="An out of the world project"
-          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut ex mi. Nulla a lacinia nisl. Quisque scelerisque velit elit, id imperdiet ipsum malesuada maximus. Aliquam eleifend ut sem id interdum. Duis ornare mollis pretium. Vestibulum finibus sem non posuere accumsan. Ut elementum ornare suscipit."
+          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut ex mi. Nulla a lacinia nisl. Quisque scelerisque velit elit, id imperdiet ipsum malesuada maximus."
           previewUrl="https://via.placeholder.com/1600x900"
           alt="preview"
           liveUrl="#"
