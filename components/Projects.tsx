02deb71889e1da2051983f3b45d3f261834be51d
Subho <contact.subhoghosh@gmail.com>
2022-11-15 21:46:21 +0530

    feat: fill projects section with details

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -13,8 +13,8 @@ export default function Projects() {
           desc="A blazing fast application to convert your tweet into beautiful images. Have all the configuration options you need at your fingertip. Simple by default, powerful when neeeded."
           previewUrl="/laureate.png"
           alt="preview"
-          liveUrl="#"
-          codeUrl="https://github.com/subhoghoshX/portfolio"
+          liveUrl="https://laureate.netlify.app"
+          codeUrl="https://github.com/subhoghoshX/laureate"
           isOpenSource={false}
         />
         <Project
@@ -41,21 +41,21 @@ export default function Projects() {
       </h3>
       <div className="mt-10 space-y-14">
         <Project
-          title="An out of the world project"
-          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut ex mi. Nulla a lacinia nisl. Quisque scelerisque velit elit, id imperdiet ipsum malesuada maximus."
-          previewUrl="https://via.placeholder.com/1600x900"
+          title="Create Tailwind"
+          desc="A commandline tool to get started with TailwindCSS and TypeScript. It uses popular scaffolding tools like create-next-app or create-vite to create and configure you project."
+          previewUrl="/create-tw.gif"
           alt="preview"
-          liveUrl="#"
-          codeUrl="https://github.com/subhoghoshX/portfolio"
+          liveUrl="https://github.com/AndrejJurkin/create-tw"
+          codeUrl="https://github.com/AndrejJurkin/create-tw/pulls?q=is%3Apr+is%3Aclosed+author%3AsubhoghoshX"
           isOpenSource={false}
         />
         <Project
-          title="An out of the world project"
-          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut ex mi. Nulla a lacinia nisl. Quisque scelerisque velit elit, id imperdiet ipsum malesuada maximus."
-          previewUrl="https://via.placeholder.com/1600x900"
+          title="Defaang"
+          desc="A website that curates recently-asked interview questions from FAANG+ to help people practice & prep! It can be seen as an alternative to LeetCode and Glassdoor."
+          previewUrl="/defaang.png"
           alt="preview"
-          liveUrl="#"
-          codeUrl="https://github.com/subhoghoshX/portfolio"
+          liveUrl="https://defaang.io/"
+          codeUrl="https://github.com/csdojo-defaang/defaang/pulls?q=is%3Apr+is%3Aclosed+author%3AsubhoghoshX"
           isOpenSource={false}
         />
         <Project
