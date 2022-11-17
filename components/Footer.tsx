8f1a276eb92c0ae99b8f36e9617b927ac75c3219
Subho <contact.subhoghosh@gmail.com>
2022-11-17 12:56:18 +0530

    feat: hover effect on footer icons, increase vertical padding

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,28 +4,28 @@ export default function Footer() {
   return (
     <footer className="relative mt-20">
       <div className="absolute -top-4 -right-12 -left-12 h-px bg-slate-900/[0.1] [mask-image:linear-gradient(to_right,transparent,white_4rem,white_calc(100%-4rem),transparent)] dark:bg-cyan-900"></div>
-      <div className="py-6">
-        <div className="flex justify-center gap-x-8">
+      <div className="pt-6 pb-8">
+        <div className="flex justify-center gap-x-8 text-zinc-600 dark:text-zinc-400">
           <a
             href="https://twitter.com/subhoghosh_"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <SiTwitter className="h-6 w-8" />
+            <SiTwitter className="h-6 w-8 hover:text-zinc-900 dark:hover:text-zinc-300" />
           </a>
           <a
             href="https://github.com/subhoghoshX"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <SiGithub className="h-6 w-6" />
+            <SiGithub className="h-6 w-6 hover:text-zinc-900 dark:hover:text-zinc-300" />
           </a>
           <a
             href="https://www.linkedin.com/in/subho-ghosh-7a59aa250/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <SiLinkedin className="h-6 w-6" />
+            <SiLinkedin className="h-6 w-6 hover:text-zinc-900 dark:hover:text-zinc-300" />
           </a>
         </div>
 
