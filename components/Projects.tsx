78a3b174d10c317e6ca4ade51f903227a914af04
Subho <contact.subhoghosh@gmail.com>
2023-03-10 22:21:23 +0530

    add projects UnrealTime & Teleport

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -26,6 +26,24 @@ export default function Projects() {
           codeUrl="https://github.com/subhoghoshX/execute"
           isOpenSource={false}
         />
+        <Project
+          title="Unreal Time"
+          desc="A secure peer to peer video conferencing application with the ability to chat and share screen."
+          previewUrl="/unrealtime.png"
+          alt="preview"
+          liveUrl="https://unrealtime.onrender.com"
+          codeUrl="https://github.com/subhoghoshX/UnrealTime"
+          isOpenSource={false}
+        />
+        <Project
+          title="Teleport"
+          desc="A secure peer to peer solution for filesharing. You can send any kind of file with anyone. Audio, video, pdf, images, music etc."
+          previewUrl="/teleport.png"
+          alt="preview"
+          liveUrl="https://teleport-29ju.onrender.com"
+          codeUrl="https://github.com/subhoghoshX/teleport"
+          isOpenSource={false}
+        />
       </div>
       <h3 className="mt-32 inline-block rounded-full border border-cyan-600 px-4 py-1.5 text-sm text-cyan-500 dark:border-cyan-800 dark:bg-cyan-900/30 dark:text-cyan-400">
         Opensource Contributions
